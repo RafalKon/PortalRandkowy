b561b73c9f8a920c4c036bbfa91bc8306e7b7a1a
Rafal <rafal.kon1995@gmail.com>
2020-04-09 23:40:15 +0200

    Getting started with alertify.

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +10,17 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       next => {
-        console.log('zalogowałeś się do aplikacji');
+        this.alertify.success('zalogowałeś się do aplikacji');
       },
       error => {
-        console.log('Wystąpił błąd logowania');
+        this.alertify.error('Wystąpił błąd logowania');
       }
     );
   }
@@ -30,6 +31,6 @@ export class NavComponent implements OnInit {
   }
   logout() {
       localStorage.removeItem('token');
-      console.log('Zostałeś wylogowany');
+      this.alertify.message('Zostałeś wylogowany');
   }
 }
