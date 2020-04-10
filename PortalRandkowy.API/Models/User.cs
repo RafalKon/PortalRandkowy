abc5ae0c738e9e916e08711419accb75308093e8
Rafal <rafal.kon1995@gmail.com>
2020-04-10 18:07:32 +0200

    Remade User class.

--- PortalRandkowy.API/Models/User.cs
+++ PortalRandkowy.API/Models/User.cs
@@ -1,10 +1,47 @@
+using System;
+using System.Collections.Generic;
+
 namespace PortalRandkowy.API.Models
 {
     public class User
     {
-        public int Id { get; set; } 
-        public string Username { get; set; }   
+        public int Id { get; set; }
+        public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ZodiacSign { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Growth { get; set; }
+        public string EyeColour { get; set; }
+        public string HairColour { get; set; }
+        public string MartialStatus { get; set; }
+        public string Education { get; set; }
+        public string Profession { get; set; }
+        public string Children { get; set; }
+        public string Languages { get; set; }
+        public string Motto { get; set; }
+        public string Description { get; set; }
+        public string Personality { get; set; }
+        public string LookingFor { get; set; }
+        public string Intrests { get; set; }
+        public string FreeTime { get; set; }
+        public string Sport { get; set; }
+        public string Movies { get; set; }
+        public string Music { get; set; }
+        public string Ilike { get; set; }
+        public string IDontLike { get; set; }
+        public string MakesMeLaugh { get; set; }
+        public string ItFeelsBestIn { get; set; }
+        public string FriendWouldDescribeMe { get; set; }
+        public ICollection<Photo> Photos { get; set; } 
+
+
+
+
     }
 }
