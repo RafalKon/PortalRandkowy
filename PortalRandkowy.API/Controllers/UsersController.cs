308f86c3258b4f6bfca4687c3aaca3a2fb5c78a7
Rafal <rafal.kon1995@gmail.com>
2020-04-11 20:34:30 +0200

    Learning AutoMapper.

--- PortalRandkowy.API/Controllers/UsersController.cs
+++ PortalRandkowy.API/Controllers/UsersController.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Dtos;
 
 namespace PortalRandkowy.API.Controllers
 {
@@ -11,27 +14,35 @@ namespace PortalRandkowy.API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserRepository _repo;
+        private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository repo)
+
+        public UsersController(IUserRepository repo, IMapper mapper)
         {
             _repo = repo;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers(){
-            
+        public async Task<IActionResult> GetUsers()
+        {
+
             var users = await _repo.GetUsers();
-            return Ok(users);
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(usersToReturn);
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser(int id){
+        public async Task<IActionResult> GetUser(int id)
+        {
 
             var user = await _repo.GetUser(id);
-            return Ok(user);
+            var userToReturn = _mapper.Map<UserForDetailsDto>(user);
+            return Ok(userToReturn);
         }
 
-         
+
 
     }
 }
---
+++ PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using PortalRandkowy.API.Dtos;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>();
+            CreateMap<User, UserForDetailsDto>();
+
+        }
+
+    }
+}
