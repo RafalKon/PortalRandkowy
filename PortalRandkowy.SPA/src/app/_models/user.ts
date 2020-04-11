702e17bd9ade24c94b393566e5f9f1aab8542033
Rafal <rafal.kon1995@gmail.com>
2020-04-11 22:22:25 +0200

    Create user.ts and photo.ts model .

---
+++ PortalRandkowy.SPA/src/app/_models/user.ts
@@ -0,0 +1,39 @@
+import { Photo } from './photo';
+
+export interface User {
+    id: number;
+    username: string;
+    gender: string;
+    age: number;
+    zodiacSign: string;
+    created: Date;
+    lastActive: Date;
+    city: string;
+    country: string;
+    growth: string;
+    eyeColor: string;
+    hairColor: string;
+    martialStatus: string;
+    education: string;
+    profession: string;
+    children: string;
+    languages: string;
+    motto: string;
+    description: string;
+    personality: string;
+    lookingFor: string;
+    interests: string;
+    freeTime: string;
+    sport: string;
+    movies: string;
+    music: string;
+    ilike: string;
+    idoNotLike: string;
+    makesMeLaugh: string;
+    itFeelsBestIn: string;
+    friendeWouldDescribeMe: string;
+    photos: Photo[];
+    photoUrl: string;
+
+
+}
