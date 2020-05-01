85a33bed10ea2b42b8c260de9229f290993a6e57
Rafal <rafal.kon1995@gmail.com>
2020-05-01 13:40:20 +0200

    Fixed bug.

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule} from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 
 import { AppComponent } from './app.component';
@@ -45,8 +46,7 @@ export function tokenGetter() {
          }
       }),
       RouterModule.forRoot(appRoutes),
-      BsDropdownModule.forRoot()
-
+      BsDropdownModule.forRoot(),
    ],
    providers: [
       AuthService,
