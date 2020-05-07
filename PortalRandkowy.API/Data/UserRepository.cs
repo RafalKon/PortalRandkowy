ddc82bd99482d2ac898219b4a5b3dbc52c9c7eac
Rafal <rafal.kon1995@gmail.com>
2020-05-08 00:36:41 +0200

    Main photo Api.

--- PortalRandkowy.API/Data/UserRepository.cs
+++ PortalRandkowy.API/Data/UserRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PortalRandkowy.API.Models;
@@ -24,10 +25,14 @@ namespace PortalRandkowy.API.Data
             var users = await _context.Users.Include(p => p.Photos).ToListAsync();
             return users;
         }
-         public async Task<Photo> GetPhoto(int id)
+        public async Task<Photo> GetPhoto(int id)
         {
             var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
             return photo;
+        }
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return await _context.Photos.Where(u => u.Id == userId).FirstOrDefaultAsync(p => p.IsMain);
         }
     }
 }
