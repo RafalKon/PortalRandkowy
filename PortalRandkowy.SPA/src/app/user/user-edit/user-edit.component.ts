9b25c3b4a0d89afdbe0e3afff44d085618c2c937
Rafal <rafal.kon1995@gmail.com>
2020-05-07 11:01:18 +0200

    edit user in service. all is working now .

--- PortalRandkowy.SPA/src/app/user/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/user/user-edit/user-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -21,7 +23,10 @@ export class UserEditComponent implements OnInit {
   }
 
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute,
+    private alertify: AlertifyService,
+    private userService: UserService,
+    private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -29,9 +34,14 @@ export class UserEditComponent implements OnInit {
     });
   }
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profil pomyślnie zaktualizowany');
-    this.editForm.reset(this.user);
+
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user)
+      .subscribe(next => {
+        this.alertify.success('Profil pomyślnie zaktualizowany');
+        this.editForm.reset(this.user);
+      }, error => {
+        this.alertify.error(error);
+      });
   }
 
 }
