7db5526a0268d73fc33609b1e794da871503e5bf
Rafal <rafal.kon1995@gmail.com>
2020-05-08 18:22:54 +0200

    next part .

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,32 +10,35 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-
   @Output() cancelRegister = new EventEmitter();
-
   model: any = {};
   registerForm: FormGroup;
-
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      username: new FormControl('Podaj nazwę użytkownika', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
       confirmPassword: new FormControl('', Validators.required)
-    });
+    }, this.passworMatchValidator);
   }
 
-   register() {
-  //   this.authService.register(this.model).subscribe(() => {
-  //     this.alertify.success('rejestracja udana');
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  console.log(this.registerForm.value);
+  passworMatchValidator(fg: FormControl) {
+    return fg.get('password').value === fg.get('confirmPassword').value ? null : { mismatch: true };
   }
+
+  register() {
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('rejestracja udana');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value);
+  }
+
   cancel() {
     this.cancelRegister.emit(false);
   }
+
 }
