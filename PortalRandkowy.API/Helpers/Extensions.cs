6d24b1c2c47f47d2d0dcf9e3be5528702bf3875d
Rafal <rafal.kon1995@gmail.com>
2020-05-09 23:07:55 +0200

    Pagination helper.

--- PortalRandkowy.API/Helpers/Extensions.cs
+++ PortalRandkowy.API/Helpers/Extensions.cs
@@ -1,28 +1,34 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace PortalRandkowy.API.Helpers
 {
     public static class Extensions
     {
-        public static int CalculateAge(this DateTime dateTime)
+        public static int CalculateAge(this DateTime datetime)
         {
+            var age = DateTime.Today.Year - datetime.Year;
 
-            var age = DateTime.Today.Year - dateTime.Year;
-            if (dateTime.AddYears(age) > DateTime.Today)
-            {
+            if(datetime.AddYears(age) > DateTime.Today)
                 age--;
-            }
+            
             return age;
-
         }
-          public static void AddApplicationError(this HttpResponse response, string message)
+
+        public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Orgin", "*");
         }
 
+        public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
 
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
     }
 }
