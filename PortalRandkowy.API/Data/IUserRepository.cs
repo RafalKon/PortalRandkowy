750d77dd16c94253527bcbe6a7fd50e002214931
Rafal <rafal.kon1995@gmail.com>
2020-05-09 23:13:44 +0200

    pagination last part.

--- PortalRandkowy.API/Data/IUserRepository.cs
+++ PortalRandkowy.API/Data/IUserRepository.cs
@@ -1,15 +1,17 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using PortalRandkowy.API.Helpers;
 using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Data
 {
     public interface IUserRepository : IGenericRepository
     {
-        Task<IEnumerable<User>> GetUsers();
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
+        Task<PagedList<User>> GetUsers(UserParams userParams);
+
 
 
 
