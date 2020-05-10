5a4bb259019bdbb18274c2a8259bd76082e153b4
Rafal <rafal.kon1995@gmail.com>
2020-05-10 03:51:29 +0200

    Create message controller.

--- PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
+++ PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
@@ -33,6 +33,8 @@ namespace PortalRandkowy.API.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<UserForRegisterDto, User>();
+            CreateMap<MessageForCreationDto, Message>().ReverseMap();
+
 
 
 
