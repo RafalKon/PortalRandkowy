598b042f8e2be9876cfa41e83563bd2387bb01ca
Rafal <rafal.kon1995@gmail.com>
2020-05-10 03:02:37 +0200

    add method to like.

--- PortalRandkowy.API/Data/IUserRepository.cs
+++ PortalRandkowy.API/Data/IUserRepository.cs
@@ -11,6 +11,7 @@ namespace PortalRandkowy.API.Data
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
         Task<PagedList<User>> GetUsers(UserParams userParams);
+        Task<Like> GetLike(int userId, int recipientId);
 
 
 
